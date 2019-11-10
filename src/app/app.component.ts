a66742741e339a5d015baefd0c94af91327d4835
Ivy Walobwa <ivywalobwa@gmail.com>
2019-11-10 22:23:36 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RockPaperScissors';
+  userScore = 0;
+  compScore = 0;
+  userSelected: string;
+  compSelected: string;
+  action: string;
+  status: string;
+  compWeapons = [
+    'rock',
+    'paper',
+    'scissors'
+  ];
+
+  userPick(userWeapon: string): void {
+    this.userSelected = userWeapon;
+    console.log( this.userSelected);
+    setTimeout( () => {
+      const randomNum = Math.floor(Math.random() * 3);
+      this.compSelected = this.compWeapons[randomNum];
+      console.log(this.compSelected);
+      this.checkResult();
+    }, 1000);
+  }
+
+  clearField() {
+    setTimeout(() => {
+      this.status = '';
+      this.userSelected = '';
+      this.compSelected = '';
+    }, 1500);
+  }
+
+  win(user, comp) {
+    this.userScore ++;
+    this.userSelected = user;
+    this.compSelected = comp;
+    this.action = 'beats';
+    this.status = '. You win!';
+    this.clearField();
+  }
+
+
+  lose(user, comp) {
+    this.compScore ++;
+    this.userSelected = user;
+    this.compSelected = comp;
+    this.action = 'loses to';
+    this.status = '. You lose!';
+    this.clearField();
+  }
+
+  draw(user, comp) {
+    this.userSelected = user;
+    this.compSelected = comp;
+    this.action = 'and';
+    this.status = '. You draw!';
+    this.clearField();
+  }
+
+  checkResult() {
+    const userChoice = this.userSelected;
+    const compChoice = this.compSelected;
+    switch (userChoice + compChoice) {
+      case 'rockscissors':
+      case 'paperrock':
+      case 'scissorspaper':
+        this.win(userChoice, compChoice);
+        break;
+      case 'rockpaper':
+      case 'scissorsrock':
+      case 'paperscissors':
+        this.lose(userChoice, compChoice);
+        break;
+      default:
+        this.draw(userChoice, compChoice);
+        break;
+    }
+
+  }
 }
